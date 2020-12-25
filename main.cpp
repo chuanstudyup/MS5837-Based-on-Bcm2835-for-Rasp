25afa913aee46d5053129768b517842e165ff88c
LC <liuchuanstudy@outlook.com>
2020-12-25 10:50:48 +0800

    i to I

--- main.cpp
+++ main.cpp
@@ -6,11 +6,11 @@ int main()
 	MS5837 ms5837;
 	if(!ms5837.init())
 	{
-		cout<<"MS5837 init Failed"<<endl;
+		cout<<"MS5837 Init Failed"<<endl;
 		exit(-1);
 	}
 	else
-		cout<<"MS5837 init OK"<<endl;
+		cout<<"MS5837 Init OK"<<endl;
 	while(1)
 	{
 		ms5837.read();
